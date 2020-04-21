5737d76b608a0e6b53a41fcf04c05ac615492d96
Crowdbotics <team@crowdbotics.com>
2020-04-21 04:12:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/powerhungermania_16096/MainActivity.java
@@ -0,0 +1,15 @@
+package com.powerhungermania_16096;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "powerhungermania_16096";
+    }
+}
